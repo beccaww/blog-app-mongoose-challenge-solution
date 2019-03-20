213087d6b8b47282e306c992feb5cf6ab7aa6f33
Rebecca Bob <rebecca.rupel@gmail.com>
2019-03-20 16:47:12 -0700

    added POST endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -103,6 +103,42 @@ describe('blog posts API resource', function () {
     });
 
     describe('POST endpoint', function () {
+
+        it('should add a new blog post', function () {
+            
+            const newPost = {
+                title: faker.lorem.sentence(), 
+                author: {
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.lastName(), 
+                },
+                content: faker.lorem.text()
+            };
+
+            return chai.request(app)
+                .post('/posts')
+                .send(newPost)
+                .then(function (res) {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.include.keys('id', 'title', 'content', 'author', 'created');
+                    res.body.title.should.equal(newPost.title);
+                    res.body.id.should.not.be.null;
+                    res.body.author.should.equal(`${newPost.author.firstName} ${newPost.author.lastName}`);
+                    res.body.content.should.equal(newPost.content);
+                    return BlogPost.findById(res.body.id);  
+                })
+                .then(function (post) {
+                    post.title.should.equal(newPost.title);
+                    post.content.should.equal(newPost.content);
+                    post.author.firstName.should.equal(newPost.author.firstName);
+                    post.author.lastName.should.equal(newPost.author.lastName);
+                  });
+        });
+    });
+
+    describe('PUT endpoint', funtion () {
         
     })
 })
