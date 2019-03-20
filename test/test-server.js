76c72db64cc1788dfc65324601d64e22641e87db
Rebecca Bob <rebecca.rupel@gmail.com>
2019-03-20 16:17:00 -0700

    set up and added tear down and seed db

---
+++ test/test-server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const chai = require('chai'); 
+const chaiHttp = require('chai-http');
+const faker = require('faker'); 
+const mongoose = require('mongoose');
+
+const should = chai.should(); 
+
+const { BlogPost } = require('../models');
+const { closeServer, runServer, app } = require('../server');
+const { TEST_DATABASE_URL } = require('../config'); 
+
+chai.use(chaiHttp);
+
+function tearDownDb(){
+    return new Promise((resolve, reject) => {
+        console.warn('Deleting database');
+        mongoose.connection.dropDatabase()
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
+}
+
+function seedBlogPostData() {
+    console.info('seeding blog post data');
+    const seedData = []; 
+    for (let i=1; i <= 10; i++) {
+        seedData.push({
+            author: {
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName()
+            },
+            title: faker.lorem.sentence(),
+            content: faker.lorem.text()
+        });
+    }
+    return BlogPost.insertMany(seedData);
+}
+
